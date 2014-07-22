4a6b1f70efdc60e7889a8fc3a3b2e81b342b97a8
Xiyang Chen <settinghead@gmail.com>
2014-07-22 04:46:04 -0400

    XUnit xml output for Angular & aggregated metrics results

--- index.js
+++ index.js
@@ -12,6 +12,13 @@
   var protractor = require("gulp-protractor").protractor;
   var path = require("path");
   var glob = require("glob");
+  var runSequence = require("run-sequence");
+  var mkdirp = require("mkdirp");
+  var uuid = require('node-uuid');
+  var junitParser = require('junit-xml-parser').parser;
+  var fs = require("fs");
+  var xml2js = require('xml2js');
+  var async = require("async");
 
   var httpServer;
 
@@ -80,13 +87,72 @@
       },
       testE2EAngular: function (options) {
         options = options || {};
-        return function () {
+        var ensureReportDirectory = function (cb) {
+          mkdirp('./reports', function (err) {
+            cb(err);
+          });
+        };
+
+        var runAngularTest = function () {
           return gulp.src(options.src || ["./test/e2e/*scenarios.js"])
             .pipe(protractor({
                 configFile: options.configFile || path.join(__dirname, "protractor.conf.js"),
                 args: ["--baseUrl", options.baseUrl || "http://127.0.0.1:8099/src/settings-e2e.html"]
             }))
             .on("error", function (e) { gutil.log(e); throw e; });
+        };
+
+        var id = uuid.v1();
+        gulp.task(id + ":ensureReportDirectory", ensureReportDirectory);
+        gulp.task(id + ":runAngularTest", runAngularTest);
+
+        return function (cb) {
+          runSequence(
+            id + ":ensureReportDirectory",
+            id + ":runAngularTest", cb
+          )
+        };
+      },
+      metrics: function (options) {
+        options = options || {};
+        return function(cb) {
+          var glob = require("glob");
+
+          // options is optional
+          glob("reports/*xunit.xml", options, function (er, files) {
+            async.map(files, function (file, mapCallback) {
+              var parser = new xml2js.Parser();
+              gutil.log("Processing file", file, "...");
+              var result = parser.parseString(fs.readFileSync(file), function (err, result) {
+                mapCallback(err, {
+                  tests: result['testsuite']['$']['tests'],
+                  failures: result['testsuite']['$']['failures'],
+                  errors: result['testsuite']['$']['errors'],
+                  skipped: result['testsuite']['$']['skipped']
+                });
+              });
+            },
+            function (err, results) {
+              if(err) {cb(err); }
+              else {
+                async.reduce(results, {tests: 0, failures: 0, errors: 0, skipped: 0}, function (memo, item, callback){
+
+                  callback(null, {
+                    tests: memo.tests + (parseInt(item.tests) || 0),
+                    failures: memo.failures + (parseInt(item.failures) || 0),
+                    errors: memo.errors + (parseInt(item.errors) || 0),
+                    skipped: memo.skipped + (parseInt(item.skipped) || 0)
+                  });
+                },
+                function (err, result){
+                  if(result) {
+                    gutil.log('Aggregated metrics result:', result);
+                    fs.writeFileSync("reports/metrics.json", JSON.stringify(result, null, 2)); }
+                  cb(err, result);
+                });
+              }
+            });
+          });
         };
       }
     }
