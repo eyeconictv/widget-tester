1a896986606e9e8248be244bf0a9197ed77d4350
Xiyang Chen <settinghead@gmail.com>
2014-07-28 02:17:07 -0400

    Integrate karma test metrics

--- karma.conf.js
+++ karma.conf.js
@@ -1,20 +1,39 @@
+var path = require("path");
+
 module.exports = function(config){
   config.set({
 
     autoWatch : false,
 
     frameworks: ["mocha", "chai", "chai-as-promised"],
-    
+
     browsers : ["PhantomJS"],
 
-    reporters: ["progress"],
+    preprocessors : {
+      "web/script/**/*.js": "coverage"
+    },
+
+    reporters: ["progress", "junit", "coverage"],
 
     plugins : [
+            "karma-jasmine",
             "karma-mocha",
             "karma-chai",
+            "karma-junit-reporter",
+            "karma-coverage",
             "karma-chai-plugins",
             "karma-phantomjs-launcher"
             ],
+
+    junitReporter : {
+      outputFile: path.join(__dirname, "../../reports/karma-xunit.xml")
+    },
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : "cobertura",
+      dir : path.join(__dirname, "../../reports/coverage")
+    },
 
     // web server port
     port: 9876,
