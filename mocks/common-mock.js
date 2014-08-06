3825262cdc616ba3fe9dbfcdbfb2c4a28b9e3083
Alex Deaconu <alex.deaconu@risevision.com>
2014-08-06 11:53:38 -0400

    Split out common.translate module

--- mocks/common-mock.js
+++ mocks/common-mock.js
@@ -4,17 +4,17 @@ var i18n = {
   }
 };
 
-angular.module('risevision.widget.common', [], function() {
+angular.module("risevision.widget.common", ["risevision.widget.common.translate"]);
 
-})
-// mock the translate filter
-  .filter('translate', function () {
-    return function (val) {
-      return val;
-    };
-  })
-  .service('i18nLoader', [function () {
+angular.module("risevision.widget.common.translate", [])
+  .service("i18nLoader", function () {
     this.get = function () {
       return { then: function(cb) { cb(); }};
     };
-  }]);
+  })
+  // mock the translate filter
+  .filter("translate", function () {
+    return function (val) {
+      return val;
+    };
+  });
