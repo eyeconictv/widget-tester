729fd09a805da667dca75e08118db11ec331171f
Alex Deaconu <alex.deaconu@gmail.com>
2015-01-02 14:00:01 -0500

    [fix] Added common-i18n mock
    Removed widget.common.translate mock

--- mocks/common-mock.js
+++ mocks/common-mock.js
@@ -4,22 +4,11 @@ var i18n = {
   }
 };
 
-angular.module("risevision.widget.common", ["risevision.widget.common.translate"]);
-
-angular.module("risevision.widget.common.translate", [])
+angular.module("risevision.widget.common", [])
   .service("i18nLoader", function () {
     this.get = function () {
       return { then: function(cb) { cb(); }};
     };
-  })
-  // mock the translate filter
-  .filter("translate", function () {
-    return function (val) {
-      return val;
-    };
   });
 
-angular.module('risevision.common.i18n.config', [])
-  .constant('LOCALES_PREFIX',
-    '/components/rv-common-i18n/dist/locales/translation_')
-  .constant('LOCALES_SUFIX', '.json');
+angular.module('risevision.common.i18n', []);
