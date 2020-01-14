acc478da0287fb665730379d1c136e3c05de14c0
Alex Deaconu <alex.deaconu@gmail.com>
2020-01-14 10:44:44 -0500

    Remove disable web security flag (#123)
    This is causing issues with setting cookies

--- protractor.conf.js
+++ protractor.conf.js
@@ -35,7 +35,7 @@ exports.config = {
       prompt_for_download: false
     },
     chromeOptions: {
-      args: ['--disable-web-security'],
+      // args: ['--disable-web-security'],
       prefs: {
         'download.default_directory': process.cwd() + '/tmp'
       }
